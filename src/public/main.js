d5644c1de874d7cea900f706b43d17f3aff0436a
DeRep311 <endcusatt26@gmail.com>
2022-09-16 18:38:35 -0300

    1.1.0

--- src/public/main.js
+++ src/public/main.js
@@ -5,11 +5,11 @@ let mensaje = document.getElementById('mensaje')
 let chat = document.getElementById('chat')
 let btn = document.getElementById('envio')
 function obtenerPlantilla(productos){
-    return fetch('./plantillas/tabla-productos.hbs')
+    return fetch('plantillas/tabla-productos.hbs')
     .then(response=> response.text())
     .then(plantilla=>{
         const plantillaHBS= Handlebars.compile(plantilla)
-        const htmlCompleto= plantillaHBS(productos)
+        const htmlCompleto= plantillaHBS({productos})
         return htmlCompleto
     })
 }
@@ -17,6 +17,8 @@ socket.on('productos', async productos=>{
     const html= await obtenerPlantilla(productos)
     document.getElementById('productos').innerHTML=html
 })
+
+
 btn.addEventListener('click', () => {
     socket.emit('newMessage', {
         Email: email.value,
--- src/sockets.js
+++ src/sockets.js
@@ -1,19 +1,19 @@
 import * as fs from "fs";
 
-export default (io)=>{
-const messages=[]
-const productos=[{name:"shampoo", price:43, img:"https://cdn-icons-png.flaticon.com/512/1828/1828108.png"}]
-io.on("connection",socket=>{
-    console.log('new user connected');
-   
-    socket.on('newMessage',message=>{
-        messages.push(message)
-        console.log(messages);
-        io.sockets.emit("chatMessage",message)
-        fs.writeFile('./chat.txt', JSON.stringify(messages, null, 2),'utf-8',err=>console.log(err))
+export default (io) => {
+    const messages = []
+    const productos = [{ name: "shampoo", price: 43, img: "https://cdn-icons-png.flaticon.com/512/1828/1828108.png" }, { name: "shampeo", price: 42, img: "https://cdn-icons-png.flaticon.com/512/1828/1828108.png" }, { name: "shdfsmpoo", price: 45, img: "https://cdn-icons-png.flaticon.com/512/1828/1828108.png" }]
+    io.on("connection", socket => {
+        console.log('new user connected');
+
+        socket.on('newMessage', message => {
+            messages.push(message)
+            console.log(messages);
+            io.sockets.emit("chatMessage", message)
+            fs.writeFile('./chat.txt', JSON.stringify(messages, null, 2), 'utf-8', err => console.log(err))
+        })
+        socket.emit('productos', productos)
     })
-    socket.emit('productos',productos)
-})
 
 
 }   
